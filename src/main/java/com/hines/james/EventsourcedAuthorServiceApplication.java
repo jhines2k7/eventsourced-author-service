cb2b35b87aa6ed3f0813c4b7621267516cab14c8
James Hines <jameshines10@gmail.com>
2017-09-11 07:24:15 -0400

    Initial commit

---
+++ src/main/java/com/hines/james/EventsourcedAuthorServiceApplication.java
@@ -0,0 +1,12 @@
+package com.hines.james;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsourcedAuthorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsourcedAuthorServiceApplication.class, args);
+	}
+}
